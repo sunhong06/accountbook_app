150472a87b5d54ac527f6aff9cf0954f69601f85
sunhong06 <eoaud447@naver.com>
2023-12-12 15:11:39 +0900

    feat: header, Container컴포넌트 작업

---
+++ src/components/common/layout/Container.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import defaultTheme from '@/styles/theme/defaultTheme';
+import styled from 'styled-components';
+
+const Root = styled.div`
+  width: 100%;
+  height: 100%;
+  padding-left: ${defaultTheme.padding.container}px;
+  padding-right: ${defaultTheme.padding.container}px;
+`;
+
+const Container = ({ children }: { children: React.ReactNode }) => {
+  return <Root>{children}</Root>;
+};
+
+export default Container;
---
+++ src/components/global/Header.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import Link from 'next/link';
+import { useState } from 'react';
+import styled from 'styled-components';
+import Button from '../common/Button';
+import { flexbox } from '@/styles/mixins/flexbox';
+import defaultTheme from '@/styles/theme/defaultTheme';
+import Container from '../common/layout/Container';
+
+const HeaderContent = styled.header`
+  ${flexbox('space-between', 'center')};
+  color: ${defaultTheme.color.WHILE};
+  height: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+`;
+const StyleLink = styled(Link)`
+  color: ${defaultTheme.color.WHILE};
+  font-size: 12px;
+  padding-right: 10px;
+`;
+const HeaderWrapper = styled.div`
+  border-bottom: 1px solid #2f2f2f;
+  height: 70px;
+`;
+
+const Header = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  const LogoutHandler = () => {};
+
+  return (
+    <HeaderWrapper>
+      <Container>
+        <HeaderContent>
+          <Title>COIN MOA</Title>
+          {isLogin ? (
+            <>
+              <Button disabled={false} onClick={LogoutHandler}>
+                로그아웃
+              </Button>
+            </>
+          ) : (
+            <div>
+              <StyleLink href={'/Login'}>로그인</StyleLink>
+              <StyleLink href={'/signUp'}>회원가입</StyleLink>
+            </div>
+          )}
+        </HeaderContent>
+      </Container>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
