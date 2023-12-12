2c6821393f6ed1b8dbf63adfd0fe61010b42d46e
sunhong06 <eoaud447@naver.com>
2023-12-12 15:14:47 +0900

    style: 공통style 지정

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,100 @@
+'use client'
+
+import { createGlobalStyle } from "styled-components";
+import defaultTheme from '@/styles/theme/defaultTheme'
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+    * {   
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+        outline: none;
+        -webkit-tap-highlight-color: transparent;
+        -webkit-touch-callout: none;
+        -ms-overflow-style: none; // IE and Edge
+        scrollbar-width: none; // Firefox
+
+        &::before,
+        &::after {
+            box-sizing: border-box;
+        }
+    }
+
+    img {
+        border: 0;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    select,
+    input,
+    textarea {
+        font-size: 16px;
+    }
+
+    input,
+    button {
+        -webkit-appearance: none;
+    }
+
+    input::-webkit-contacts-auto-fill-button {
+        position: absolute;
+        right: 0;
+        display: none !important;
+        pointer-events: none;
+        visibility: hidden;
+    }
+
+    input {
+        background-color: transparent;
+        background-image: none;
+        border: 0;
+    }
+
+    button {
+        cursor: pointer;
+        user-select: none;
+        background-color: transparent;
+        border: 0;
+
+        &:disabled {
+            cursor: not-allowed;
+        }
+    }
+
+    input:is([type='button'], [type='submit'], [type='reset']),
+    input[type='file']::file-selector-button,
+    button {
+        color: initial;
+    } // override safari default blue
+
+    html,
+    body,
+    button,
+    input,
+    textarea {
+        font-family: Poppins, Arial, PingFangSC-Regular, 'Microsoft YaHei', sans-serif;
+    }
+
+    body{
+        background-color: ${defaultTheme.color.MAIN_BG};
+    }
+
+    ::-webkit-scrollbar {
+        background-color: transparent;
+        width: 7px;
+        height: 7px;
+    }
+    ::-webkit-scrollbar-thumb {
+        border-radius: 7px;
+        background: #2E2C30;
+    }
+    
+`;
+
+
+export default GlobalStyle;
---
+++ src/styles/theme/defaultTheme.ts
@@ -0,0 +1,13 @@
+
+
+const defaultTheme = {
+    color:{
+        MAIN_BG:'#151617',
+        WHILE:'#FFFFFF'
+    },
+    padding:{
+        container:30,
+    }
+}
+
+export default defaultTheme;
